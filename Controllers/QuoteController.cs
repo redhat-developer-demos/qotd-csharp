f9f170785b2a171bd2f99319e81e1a3d4a23efc8
Don Schenck <don.schenck@gmail.com>
2022-01-28 15:51:44 -0500

    fixes

--- Controllers/QuoteController.cs
+++ Controllers/QuoteController.cs
@@ -16,13 +16,20 @@ namespace qotd_csharp.Controllers
 
 
         private Quote[] _quotes() {
-            Quote[] q = new Quote[6];
+            Quote[] q = new Quote[12];
             q[0] = new Quote {Id = 0, Quotation = "I got a fever, and the only prescription is more cowbell!", Author = "Will Ferrell"};
             q[1] = new Quote{Id = 1, Quotation ="Knowledge is power.", Author ="Francis Bacon"};
             q[2] = new Quote{Id = 2, Quotation = "Life is really simple, but we insist on making it complicated.", Author = "Confucius"};
 	        q[3] = new Quote{Id = 3, Quotation = "This above all, to thine own self be true.", Author = "William Shakespeare"};
 	        q[4] = new Quote{Id = 4, Quotation = "Never complain. Never explain.", Author = "Katharine Hepburn"};
 	        q[5] = new Quote{Id = 5, Quotation = "Do be do be dooo.", Author = "Frank Sinatra"};
+            q[6] = new Quote {Id = 0, Quotation = "It's time to start living the life you've imagined.", Author = "Henry James"};
+            q[7] = new Quote{Id = 1, Quotation ="Courage is grace under pressure.", Author ="Ernest Hemingway"};
+            q[8] = new Quote{Id = 2, Quotation = "Either you run the day or the day runs you.", Author = "Jim Rohn"};
+	        q[9] = new Quote{Id = 3, Quotation = "Exercise is to the body as reading is to your mind.", Author = "Joseph Addison"};
+	        q[10] = new Quote{Id = 4, Quotation = "The universe rearranges itself to accommodate your picture of reality.", Author = "Unknown"};
+	        q[11] = new Quote{Id = 5, Quotation = "All our dreams can come true, if we have the courage to pursue them.", Author = "Walt Disney"};
+
             return q;
         }
 
@@ -55,7 +62,7 @@ namespace qotd_csharp.Controllers
 
         [HttpGet("version")]
         public string Version() {
-            return "1.0.2";
+            return "2.0.0";
         }
 
         [HttpGet("writtenin")]
